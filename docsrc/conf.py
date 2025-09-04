c6c6c3f43fcb027c27ca80dd4eb03635dd6a1c80
fzhu2e <fzhu2e@outlook.com>
2025-09-04 14:30:06 -0600

    update docs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -52,8 +52,6 @@ extensions = [
     # 'sphinx_gallery.load_style',
 ]
 nbsphinx_allow_errors = True
-nbsphinx_input_prompt = "%s"
-nbsphinx_output_prompt = "%s"
 
 html_logo = 'x4c-logo.png'
 html_favicon = 'x4c-logo.png'
