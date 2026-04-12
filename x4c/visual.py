99a035b7bb6362b98293726930f47c6210828588
Feng Zhu <fzhu2e@outlook.com>
2026-04-11 19:33:57 -0600

    add viz styles

--- x4c/visual.py
+++ x4c/visual.py
@@ -89,25 +89,30 @@ def savefig(fig, path, verbose=True, **kws):
 
 def set_style(style='journal', font_scale=1.0):
     ''' Modify the visualization style
-    
+
     This function is inspired by [Seaborn](https://github.com/mwaskom/seaborn).
     See a demo in the example_notebooks folder on GitHub to look at the different styles
-    
+
     Parameters
     ----------
-    
-    style : {journal, web, matplotlib, _spines, _nospines,_grid,_nogrid}
+
+    style : {journal, web, nature, agu, presentation, dark, minimal, matplotlib, _spines, _nospines, _grid, _nogrid}
         set the styles for the figure:
             - journal (default): fonts appropriate for paper
             - web: web-like font (e.g. ggplot)
+            - nature: clean style inspired by Nature/Science figures with Helvetica-like fonts
+            - agu: style following AGU journal conventions with minor ticks and enclosed axes
+            - presentation: bold, high-contrast style for talks and posters
+            - dark: modern dark theme for screen display and dashboards
+            - minimal: ultra-clean style with thin lines and open layout
             - matplotlib: the original matplotlib style
             In addition, the following options are available:
             - _spines/_nospines: allow to show/hide spines
             - _grid/_nogrid: allow to show gridlines (default: _grid)
-    
+
     font_scale : float
-        Default is 1. Corresponding to 12 Font Size. 
-    
+        Default is 1. Corresponding to 12 Font Size.
+
     '''
     font_dict = {
         'font.size': 12,
@@ -192,6 +197,266 @@ def set_style(style='journal', font_scale=1.0):
             'ytick.major.width': 1.25,
             'xtick.minor.width': 0,
             'ytick.minor.width': 0,
+        })
+    elif 'nature' in style:
+        style_dict.update({
+            'figure.facecolor': 'white',
+            'figure.dpi': 150,
+
+            'axes.axisbelow': True,
+            'axes.facecolor': 'white',
+            'axes.edgecolor': '#333333',
+            'axes.grid': False,
+            'axes.linewidth': 0.8,
+            'axes.prop_cycle': mpl.cycler('color', [
+                '#0072B2', '#D55E00', '#009E73', '#CC79A7',
+                '#F0E442', '#56B4E9', '#E69F00', '#000000',
+            ]),
+
+            'xtick.direction': 'in',
+            'ytick.direction': 'in',
+            'xtick.top': True,
+            'ytick.right': True,
+
+            'font.family': 'sans-serif',
+            'font.sans-serif': ['Helvetica', 'Arial', 'DejaVu Sans', 'sans-serif'],
+
+            'axes.spines.left': True,
+            'axes.spines.bottom': True,
+            'axes.spines.right': True,
+            'axes.spines.top': True,
+
+            'legend.frameon': False,
+            'legend.handlelength': 1.5,
+
+            'lines.linewidth': 1.5,
+            'lines.markersize': 5,
+            'patch.linewidth': 0.8,
+
+            'xtick.major.width': 0.8,
+            'ytick.major.width': 0.8,
+            'xtick.minor.width': 0.5,
+            'ytick.minor.width': 0.5,
+            'xtick.major.size': 4,
+            'ytick.major.size': 4,
+            'xtick.minor.size': 2,
+            'ytick.minor.size': 2,
+            'xtick.minor.visible': True,
+            'ytick.minor.visible': True,
+        })
+        font_dict.update({
+            'font.size': 10,
+            'axes.labelsize': 10,
+            'axes.titlesize': 10,
+            'xtick.labelsize': 9,
+            'ytick.labelsize': 9,
+            'legend.fontsize': 9,
+        })
+    elif 'agu' in style:
+        style_dict.update({
+            'figure.facecolor': 'white',
+            'figure.dpi': 150,
+
+            'axes.axisbelow': True,
+            'axes.facecolor': 'white',
+            'axes.edgecolor': 'black',
+            'axes.grid': False,
+            'axes.linewidth': 1.0,
+
+            'xtick.direction': 'in',
+            'ytick.direction': 'in',
+            'xtick.top': True,
+            'ytick.right': True,
+
+            'font.family': 'sans-serif',
+            'font.sans-serif': ['Helvetica', 'Arial', 'DejaVu Sans', 'sans-serif'],
+
+            'axes.spines.left': True,
+            'axes.spines.bottom': True,
+            'axes.spines.right': True,
+            'axes.spines.top': True,
+
+            'legend.frameon': True,
+            'legend.fancybox': False,
+            'legend.edgecolor': 'black',
+            'legend.framealpha': 1.0,
+
+            'lines.linewidth': 1.5,
+            'lines.markersize': 5,
+            'patch.linewidth': 1,
+
+            'xtick.major.width': 1.0,
+            'ytick.major.width': 1.0,
+            'xtick.minor.width': 0.6,
+            'ytick.minor.width': 0.6,
+            'xtick.major.size': 5,
+            'ytick.major.size': 5,
+            'xtick.minor.size': 3,
+            'ytick.minor.size': 3,
+            'xtick.minor.visible': True,
+            'ytick.minor.visible': True,
+
+            'grid.color': '#CCCCCC',
+            'grid.linestyle': ':',
+            'grid.linewidth': 0.5,
+        })
+    elif 'presentation' in style:
+        style_dict.update({
+            'figure.facecolor': 'white',
+            'figure.dpi': 100,
+
+            'axes.axisbelow': True,
+            'axes.facecolor': 'white',
+            'axes.edgecolor': '#222222',
+            'axes.grid': True,
+            'axes.linewidth': 1.5,
+            'axes.prop_cycle': mpl.cycler('color', [
+                '#2176AE', '#E84855', '#57A773', '#F9A620',
+                '#8B5CF6', '#06D6A0', '#EF476F', '#118AB2',
+            ]),
+
+            'grid.color': '#E5E5E5',
+            'grid.linestyle': '-',
+            'grid.linewidth': 0.8,
+
+            'xtick.direction': 'out',
+            'ytick.direction': 'out',
+
+            'font.family': 'sans-serif',
+            'font.sans-serif': ['Helvetica', 'Arial', 'DejaVu Sans', 'sans-serif'],
+
+            'axes.spines.left': True,
+            'axes.spines.bottom': True,
+            'axes.spines.right': False,
+            'axes.spines.top': False,
+
+            'legend.frameon': True,
+            'legend.fancybox': True,
+            'legend.edgecolor': 'none',
+            'legend.facecolor': 'white',
+            'legend.framealpha': 0.8,
+
+            'lines.linewidth': 2.5,
+            'lines.markersize': 8,
+            'patch.linewidth': 1.5,
+
+            'xtick.major.width': 1.5,
+            'ytick.major.width': 1.5,
+            'xtick.minor.width': 0,
+            'ytick.minor.width': 0,
+            'xtick.major.size': 6,
+            'ytick.major.size': 6,
+        })
+        font_dict.update({
+            'font.size': 16,
+            'axes.labelsize': 18,
+            'axes.titlesize': 20,
+            'xtick.labelsize': 14,
+            'ytick.labelsize': 14,
+            'legend.fontsize': 14,
+        })
+    elif 'dark' in style:
+        style_dict.update({
+            'figure.facecolor': '#1a1a2e',
+            'figure.edgecolor': '#1a1a2e',
+            'figure.dpi': 100,
+
+            'axes.axisbelow': True,
+            'axes.facecolor': '#16213e',
+            'axes.edgecolor': '#4a4a6a',
+            'axes.grid': True,
+            'axes.linewidth': 0.8,
+            'axes.labelcolor': '#c8c8d8',
+            'axes.prop_cycle': mpl.cycler('color', [
+                '#4CC9F0', '#F72585', '#7209B7', '#4361EE',
+                '#3A0CA3', '#06D6A0', '#FFD166', '#EF476F',
+            ]),
+
+            'grid.color': '#2a2a4a',
+            'grid.linestyle': '-',
+            'grid.linewidth': 0.5,
+
+            'xtick.direction': 'out',
+            'ytick.direction': 'out',
+            'xtick.color': '#8888a8',
+            'ytick.color': '#8888a8',
+
+            'text.color': '#d0d0e0',
+
+            'font.family': 'sans-serif',
+            'font.sans-serif': ['Helvetica', 'Arial', 'DejaVu Sans', 'sans-serif'],
+
+            'axes.spines.left': True,
+            'axes.spines.bottom': True,
+            'axes.spines.right': False,
+            'axes.spines.top': False,
+
+            'legend.frameon': True,
+            'legend.fancybox': True,
+            'legend.edgecolor': 'none',
+            'legend.facecolor': '#16213e',
+            'legend.framealpha': 0.9,
+            'legend.labelcolor': '#c8c8d8',
+
+            'lines.linewidth': 2,
+            'lines.markersize': 6,
+            'patch.linewidth': 1,
+
+            'xtick.major.width': 0.8,
+            'ytick.major.width': 0.8,
+            'xtick.minor.width': 0,
+            'ytick.minor.width': 0,
+
+            'savefig.facecolor': '#1a1a2e',
+            'savefig.edgecolor': '#1a1a2e',
+        })
+    elif 'minimal' in style:
+        style_dict.update({
+            'figure.facecolor': 'white',
+            'figure.dpi': 150,
+
+            'axes.axisbelow': True,
+            'axes.facecolor': 'white',
+            'axes.edgecolor': '#AAAAAA',
+            'axes.grid': False,
+            'axes.linewidth': 0.5,
+            'axes.prop_cycle': mpl.cycler('color', [
+                '#264653', '#2A9D8F', '#E9C46A', '#F4A261',
+                '#E76F51', '#606C38', '#283618', '#DDA15E',
+            ]),
+
+            'xtick.direction': 'out',
+            'ytick.direction': 'out',
+
+            'font.family': 'sans-serif',
+            'font.sans-serif': ['Helvetica Neue', 'Helvetica', 'Arial', 'DejaVu Sans', 'sans-serif'],
+
+            'axes.spines.left': True,
+            'axes.spines.bottom': True,
+            'axes.spines.right': False,
+            'axes.spines.top': False,
+
+            'legend.frameon': False,
+            'legend.handlelength': 1.2,
+
+            'lines.linewidth': 1.5,
+            'lines.markersize': 5,
+            'patch.linewidth': 0.5,
+
+            'xtick.major.width': 0.5,
+            'ytick.major.width': 0.5,
+            'xtick.minor.width': 0,
+            'ytick.minor.width': 0,
+            'xtick.major.size': 3,
+            'ytick.major.size': 3,
+        })
+        font_dict.update({
+            'font.size': 11,
+            'axes.labelsize': 11,
+            'axes.titlesize': 12,
+            'xtick.labelsize': 10,
+            'ytick.labelsize': 10,
+            'legend.fontsize': 10,
         })
     elif 'matplotlib' in style or 'default' in style:
         mpl.rcParams.update(inline_rc)
